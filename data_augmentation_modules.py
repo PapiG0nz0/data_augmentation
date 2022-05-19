bbd8d358cb979c565701ef673f1d1c305bc09411
PapiG0nz0 <gonzaloalcala0304@gmail.com>
2022-05-19 14:14:05 -0500

    Fixed time execution

--- data_augmentation_modules.py
+++ data_augmentation_modules.py
@@ -1,5 +1,6 @@
 import os
 from random import seed
+from timeit import timeit
 from importlib_metadata import files
 import tensorflow as tf
 from PIL import Image
@@ -7,12 +8,16 @@ import numpy as np
 import re
 import time 
 import albumentations as A
+import timeit
+
 
 
 # img = Image.open('E:\Data Augmentation\Entrenamiento\A\A_0.jpg')
-output_path = 'E:\Data Augmentation\Entrenamiento\Output\\'
-input_path = 'E:\Data Augmentation\Entrenamiento\Test\\'
+# output_path = 'E:\Data Augmentation\Entrenamiento\Output\\'
+# input_path = 'E:\Data Augmentation\Entrenamiento\Test\\'
 start_time = time.perf_counter()
+str_template_time = None
+class_name = 'A'
 
 def sorted_alphanumeric(data):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
@@ -28,62 +33,64 @@ def  get_seed():
 def save_img(img,index,output_path):
     img_arr = np.array(img)
     save_img = Image.fromarray(img_arr)
-    str_template = output_path+'PRUEBA_'+str(index)+'.jpg'
+    str_template = output_path+class_name+str(index)+'.jpg'
     save_img.save(str_template)
-    return print('Imagen: PRUEBA_'+str(index)+', guardada exitosamente')
+    return print('Imagen: '+class_name+str(index)+', guardada exitosamente')
 
-def get_img_path():
-    folder_path = 'E:\Data Augmentation\Entrenamiento\Test\\'
-    return folder_path
+def get_img_path(output_path):
+    output_path = output_path
+    return output_path
     
-def random_brightness(input_path):
-    index = 0
+def random_brightness(input_path,output_path):
+    start = time.time()
     count = sum(len(files) for _, _, files in os.walk(input_path))
+    index = count
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
-                    seed = get_seed()
-                    print(seed)
-                    img = Image.open(input_path+images)
-                    img_r_brightness = tf.image.stateless_random_brightness(img, max_delta=.4, seed=seed)
-                    save_img(img_r_brightness, index, output_path)
-                    index = index + 1
-    print("--- %s segundos en procesar #%i imagenes ---" % ((time.perf_counter() - start_time), count))
+                seed = get_seed()
+                print(seed)
+                img = Image.open(input_path+images)
+                img_r_brightness = (tf.image.stateless_random_brightness(img, max_delta=.4, seed=seed))
+                save_img(img_r_brightness, index, output_path)
+                index = index + 1
+    end = time.time()
+    exec_time = end-start
+    str_template_time = (str(exec_time)+" segundos en procesar #"+str(count)+" imagenes")
+    return str_template_time
 
-def random_contrast(input_path):
-    index = 0
+def random_contrast(input_path,output_path):
     count = sum(len(files) for _, _, files in os.walk(input_path))
+    index = count
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
                     seed = get_seed()
                     print(seed)
                     img = Image.open(input_path+images)
                     img_r_contrast = tf.image.stateless_random_contrast(img, lower = 0.3 , upper = 1, seed=seed)
                     save_img(img_r_contrast, index, output_path)
                     index = index + 1
-    print("--- %s segundos en procesar #%i imagenes ---" % ((time.perf_counter() - start_time), count))
+                    str_template_time = "--- %s segundos en procesar #%i imagenes ---" % ((time.perf_counter() - start_time), count)
+    return str_template_time
 
-def flip_left_right(input_path):
-    index = 0
+def flip_left_right(input_path, output_path):
     count = sum(len(files) for _, _, files in os.walk(input_path))
+    index = 0
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
                     seed = get_seed()
                     print(seed)
                     img = Image.open(input_path+images)
                     img_flip_left_right = tf.image.flip_left_right(img)
                     save_img(img_flip_left_right, index, output_path)
                     index = index + 1
-    print("--- %s segundos en procesar #%i imagenes ---" % ((time.perf_counter() - start_time), count))
+                    str_template_time = "--- %s segundos en procesar #%i imagenes ---" % ((time.perf_counter() - start_time), count)
+    return str_template_time
 
 def up_down(input_path):
     index = 0
     count = sum(len(files) for _, _, files in os.walk(input_path))
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
                     seed = get_seed()
                     print(seed)
                     img = Image.open(input_path+images)
@@ -92,12 +99,11 @@ def up_down(input_path):
                     index = index + 1
     print("--- %s segundos en procesar #%i imagenes ---" % ((time.perf_counter() - start_time), count))
 
-def random_hue(input_path):
+def random_hue(input_path, output_path):
     index = 0
     count = sum(len(files) for _, _, files in os.walk(input_path))
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
                     seed = get_seed()
                     print(seed)
                     img = Image.open(input_path+images)
@@ -111,7 +117,6 @@ def random_jpeg_quality(input_path):
     count = sum(len(files) for _, _, files in os.walk(input_path))
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
                     seed = get_seed()
                     print(seed)
                     img = Image.open(input_path+images)
@@ -125,7 +130,6 @@ def random_saturation(input_path):
     count = sum(len(files) for _, _, files in os.walk(input_path))
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
                     seed = get_seed()
                     print(seed)
                     img = Image.open(input_path+images)
@@ -165,7 +169,6 @@ def total_random(input_path):
     count = sum(len(files) for _, _, files in os.walk(input_path))
     for images in sorted_alphanumeric(os.listdir(input_path)):
             if(images.endswith(".jpg") or images.endswith(".png")):
-                if(index <= count):
                     seed = get_seed()
                     print(seed)
                     image = np.array(Image.open(input_path+images))
